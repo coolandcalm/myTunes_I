f7e1ff4074f6f93299635920ebba45d732b3c342
Jakub Mazur <jakubmazur2912@gmail.com>
2023-11-28 23:30:36 +0100

    First draft of layout

---
+++ src/main/java/com/example/mytunes/songController.java
@@ -0,0 +1,33 @@
+package com.example.mytunes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class songController {
+    @FXML
+    public Label songName;
+    @FXML
+    public Label artistName;
+    @FXML
+    public void addNewSong(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void playCurrentSong(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void stop(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void playPreviousSong(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void playNextSong(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void addNewPlaylist(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void switchToAllSongs(ActionEvent actionEvent) {
+    }
+}
