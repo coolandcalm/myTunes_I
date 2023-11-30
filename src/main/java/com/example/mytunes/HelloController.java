f112c0f79efa8f1f4ba6cbeae19f8130d2735d6b
Andrii Savchenko <109040480+andriiVIt@users.noreply.github.com>
2023-11-30 12:50:15 +0100

    First3

--- src/main/java/com/example/mytunes/HelloController.java
+++ src/main/java/com/example/mytunes/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.mytunes;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -7,8 +8,20 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void playlistNewButtonAction(ActionEvent event) {
+    }
+    @FXML
+    public void playlistEditButtonAction(ActionEvent event) {
+    }
+    @FXML
+    public void deleteButtonAction(ActionEvent event) {
+    }
+    @FXML
+    public void songEditButtonAction(ActionEvent event) {
+    }
+    @FXML
+    public void songNewButtonAction(ActionEvent event) {
     }
 }
