c280dad2791ed500036aaae89b40732519c34444
Andrii Savchenko <109040480+andriiVIt@users.noreply.github.com>
2023-12-07 20:55:47 +0100

    I add controller and fxml

---
+++ src/main/java/com/example/mytunes/gui/controller/NewPlayListController.java
@@ -0,0 +1,11 @@
+package com.example.mytunes.gui.controller;
+
+import javafx.event.ActionEvent;
+
+public class NewPlayListController {
+    public void btnSaveAction(ActionEvent event) {
+    }
+
+    public void cancelButtonAction(ActionEvent event) {
+    }
+}
---
+++ src/main/java/com/example/mytunes/gui/controller/PlayListEditController.java
@@ -0,0 +1,11 @@
+package com.example.mytunes.gui.controller;
+
+import javafx.event.ActionEvent;
+
+public class PlayListEditController {
+    public void btnSaveAction(ActionEvent event) {
+    }
+
+    public void cancelButtonAction(ActionEvent event) {
+    }
+}
